02d99551ff1e49dfbd66af1d3bcea35f11328ec7
Hakan Günay <hakangunay@gmail.com>
2020-08-20 23:17:07 +0300

    Files added.

---
+++ DotNet_Rfc2898_PasswordEncryption/PasswordHelper.cs
@@ -0,0 +1,35 @@
+﻿namespace DotNet_Rfc2898_PasswordEncryption
+{
+    using System;
+    using System.Security.Cryptography;
+    using System.Text;
+
+    /// <summary>The password helper.</summary>
+    public static class PasswordHelper
+    {
+        /// <summary>The encode password.</summary>
+        /// <param name="password">The password.</param>
+        /// <param name="passwordSalt">The password salt.</param>
+        /// <returns>The <see cref="string"/>.</returns>
+        public static string EncodePassword(string password, string passwordSalt)
+        {
+            var bytePassword     = Encoding.Unicode.GetBytes(password);
+            var bytePasswordSalt = Convert.FromBase64String(passwordSalt);
+
+            var byteRfc2898DeriveBytes = new Rfc2898DeriveBytes(bytePassword, bytePasswordSalt, 1000, HashAlgorithmName.SHA256);
+            var rfcHash                = byteRfc2898DeriveBytes.GetBytes(64);
+
+            return Convert.ToBase64String(rfcHash);
+        }
+
+        /// <summary>The create salt.</summary>
+        /// <returns>The <see cref="string"/>.</returns>
+        public static string CreateSaltPassword()
+        {
+            var saltBytes = new byte[64];
+            new RNGCryptoServiceProvider().GetNonZeroBytes(saltBytes);
+
+            return Convert.ToBase64String(saltBytes);
+        }
+    }
+}
---
+++ DotNet_Rfc2898_PasswordEncryption/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace DotNet_Rfc2898_PasswordEncryption
+{
+    using System;
+
+    /// <summary>The program.</summary>
+    public static class Program
+    {
+        /// <summary>The main.</summary>
+        public static void Main()
+        {
+            Console.WriteLine("RFC2898 Password Encryption");
+            Console.WriteLine("---------------------------");
+
+            Console.Write("Input your password : ");
+            var password = Console.ReadLine();
+
+            var saltPassword    = PasswordHelper.CreateSaltPassword();
+            var encodedPassword = PasswordHelper.EncodePassword(password, saltPassword);
+
+            Console.WriteLine();
+            Console.WriteLine($"Your password    : {password}");
+            Console.WriteLine($"Salt password    : {saltPassword}");
+            Console.WriteLine($"Encoded password : {encodedPassword}");
+        }
+    }
+}
